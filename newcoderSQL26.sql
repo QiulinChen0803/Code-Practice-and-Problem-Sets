3a1c4d6b428908568a5090b8af4a776d0b44de7c
Qiulin Chen <87684378+QiulinChen0803@users.noreply.github.com>
2021-08-14 16:39:15 -0500

    Add files via upload

---
+++ newcoderSQL26.sql
@@ -0,0 +1,8 @@
+SELECT d.dept_no,
+    d.dept_name,
+    t.title,
+    count(t.title) AS count
+FROM departments d
+INNER JOIN dept_emp de ON d.dept_no = de.dept_no
+INNER JOIN titles t ON de.emp_no = t.emp_no
+GROUP BY de.dept_no, t.title
